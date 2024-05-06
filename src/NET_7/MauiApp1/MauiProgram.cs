874fc1b16af2167b727420dae3ce727329314ed0
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2024-05-06 11:32:48 +0530

    Sample project to simulate the issue https://github.com/dotnet/maui/issues/22214

---
+++ src/NET_7/MauiApp1/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using CommunityToolkit.Maui.Maps;
+
+namespace MauiApp1
+{
+    public static partial class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>()
+                   .UseMauiMaps()
+                   .UseMauiCommunityToolkitMaps("<BING_MAPS_API_KEY_HERE>") // To generate a Bing Maps API Key, visit https://www.bingmapsportal.com/
+                   .ConfigureFonts(fonts =>
+                   {
+                       fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                       fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                   });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ src/NET_8/MauiApp1/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using CommunityToolkit.Maui.Maps;
+
+namespace MauiApp1
+{
+    public static partial class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>()
+// Same thing works on .NET 7 without this compiler directive workaround
+// Uncomment the compiler directive to clear the runtime error
+//#if !WINDOWS
+                   .UseMauiMaps()
+//#endif
+                   .UseMauiCommunityToolkitMaps("<BING_MAPS_API_KEY_HERE>") // To generate a Bing Maps API Key, visit https://www.bingmapsportal.com/
+                   .ConfigureFonts(fonts =>
+                   {
+                       fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                       fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                   });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
